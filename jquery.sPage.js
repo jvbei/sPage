9c8d92cb37e7962a5b1de342f54595dd357f8bf9
jvbei <yeng525@163.com>
2019-07-04 19:19:39 +0800

    优化代码结构，解决多个实例不正常的问题

--- jquery.sPage.js
+++ jquery.sPage.js
@@ -2,18 +2,17 @@
 *   分页插件
 *   by 凌晨四点半
 *   20190630
-*   v1.0.0
+*   v1.0.2
 */
 ;(function ( $, window, document, undefined ) {
     'use strict';
     var pluginName = "sPage",
-        pageNum = 1,
-        pageList = [],
         defaults = {
             page:1,
             pageSize:10,
-            listTotal:0,
+            total:0,
             showTotal:false,
+            totalTxt:"共{total}条",
             showSkip:false,
             showPN:true,
             prevPage:"上一页",
@@ -23,111 +22,121 @@
     function Plugin ( element,options ) {
         this.element = $(element);
         this.settings = $.extend( {}, defaults, options );
-        this._defaults = defaults;
-        this._name = pluginName;
+        this.pageNum = 1,
+        this.pageList = [],
         this.init();
     }
     $.extend(Plugin.prototype, {
         init: function () {
-            this.viewHtml();
-            this.clickBtn();
+            if(this.settings.total > 0){
+                this.viewHtml();
+                this.clickBtn();
+            }
         },
         creatHtml:function(i){
-            if(i==this.settings.page){
-                pageList.push('<span class="active" data-page='+i+'>'+i+'</span>');
+            if(i == this.settings.page){
+                this.pageList.push('<span class="active" data-page='+i+'>'+i+'</span>');
             }else{
-                pageList.push('<span data-page='+i+'>'+i+'</span>');
+                this.pageList.push('<span data-page='+i+'>'+i+'</span>');
             }
         },
         viewHtml:function(){
             var settings = this.settings;
-            var pageTatol = Math.ceil(settings.listTotal/settings.pageSize);
-            pageNum = settings.page;
+            var pageTatol = Math.ceil(settings.total/settings.pageSize);
+            var pageArr = [];//分页元素集合，减少dom重绘次数
+            this.pageNum = settings.page;
             this.element.empty();
+
             if(settings.showTotal){
-                this.element.append('<div class="spage-total">总计'+settings.listTotal+'条数据</div>');
+                pageArr.push('<div class="spage-total">'+settings.totalTxt.replace(/\{(\w+)\}/gi, settings.total)+'</div>');
             }
-            this.element.append('<div class="spage-number"></div>');
-            pageList = [];
+            pageArr.push('<div class="spage-number">');
+            this.pageList = [];//页码元素集合，包括上下页
             if(settings.showPN){
-                settings.page == 1?pageList.push('<span class="span-disabled" data-page="prev">'+settings.prevPage+'</span>'):pageList.push('<span data-page="prev">'+settings.prevPage+'</span>');
+                settings.page == 1?this.pageList.push('<span class="span-disabled" data-page="prev">'+settings.prevPage+'</span>'):this.pageList.push('<span data-page="prev">'+settings.prevPage+'</span>');
             }
-            if(pageTatol<=6){
+            if(pageTatol <= 6){
                 for(var i=1;i<pageTatol+1;i++){
                     this.creatHtml(i);
                 }
             }else{
-                if(settings.page<5){
+                if(settings.page < 5){
                     for(var i=1;i<=5;i++){
                         this.creatHtml(i);
                     }
-                    pageList.push('<span data-page="none">...</span>');
-                    pageList.push('<span data-page='+pageTatol+'>'+pageTatol+'</span>');
-                }else if(settings.page>pageTatol-4){
-                    pageList.push('<span data-page="1">1</span><span data-page="none">...</span>');
+                    this.pageList.push('<span data-page="none">...</span><span data-page='+pageTatol+'>'+pageTatol+'</span>');
+                }else if(settings.page > pageTatol-4){
+                    this.pageList.push('<span data-page="1">1</span><span data-page="none">...</span>');
                     for(var i=pageTatol-4;i<=pageTatol;i++){
                         this.creatHtml(i);
                     }
                 }else{
-                    pageList.push('<span data-page="1">1</span><span data-page="none">...</span>');
+                    this.pageList.push('<span data-page="1">1</span><span data-page="none">...</span>');
                     for(var i=settings.page-2;i<=Number(settings.page)+2;i++){
                         this.creatHtml(i);
                     }
-                    pageList.push('<span data-page="none">...</span><span data-page='+pageTatol+'>'+pageTatol+'</span>');
+                    this.pageList.push('<span data-page="none">...</span><span data-page='+pageTatol+'>'+pageTatol+'</span>');
                 }
             }
             if(settings.showPN){
-                settings.page == pageTatol?pageList.push('<span class="span-disabled" data-page="next">'+settings.nextPage+'</span>'):pageList.push('<span data-page="next">'+settings.nextPage+'</span>');
+                settings.page == pageTatol?this.pageList.push('<span class="span-disabled" data-page="next">'+settings.nextPage+'</span>'):this.pageList.push('<span data-page="next">'+settings.nextPage+'</span>');
             }
-            this.element.children(".spage-number").append(pageList.join(''));
+            pageArr.push(this.pageList.join(''));
+            pageArr.push('</div>');
             if(settings.showSkip){
-                this.element.append('<div class="spage-skip">跳转到&nbsp;<input type="text" name="pageNum" />&nbsp;页&nbsp;&nbsp;<span data-page="go">确定</span></div>');
+                pageArr.push('<div class="spage-skip">跳至&nbsp;<input type="text" />&nbsp;页&nbsp;&nbsp;<span data-page="go">确定</span></div>');
             }
-            this.element.children(".spage-skip").children("input").val(settings.page);
+            this.element.append(pageArr.join(''));
+            if(settings.showSkip){
+                this.element.children(".spage-skip").children("input").val(settings.page);
+            }
         },
         clickBtn:function(){
             var that = this;
             var settings = this.settings;
             var ele = this.element;
+            var pageTatol = Math.ceil(settings.total/settings.pageSize);
             this.element.off('click',"span");
             this.element.on('click',"span",function(){
                 var pageText = $(this).data("page");
-                var pageTatol = Math.ceil(settings.listTotal/settings.pageSize);
-                if(pageText == "prev"){
-                    settings.page = settings.page-1 >= 1?settings.page-1:1;
-                    pageText = settings.page;
-                }else if(pageText == "next"){
-                    settings.page = Number(settings.page)+1 <= pageTatol?Number(settings.page)+1:pageTatol;
-                    pageText = settings.page;
-                }else if(pageText == "none"){
-                    return;
-                }else if(pageText == "go"){
-                    var p = Number(ele.children(".spage-skip").children("input").val());
-                    if(/^[1-9]*$/.test(parseInt(p))){
-                        settings.page = p;
-                        pageText = p;
-                    }else{
+                switch(pageText) {
+                    case "prev":
+                        settings.page = settings.page-1 >= 1?settings.page-1:1;
+                        pageText = settings.page;
+                        break;
+                    case "next":
+                        settings.page = Number(settings.page)+1 <= pageTatol?Number(settings.page)+1:pageTatol;
+                        pageText = settings.page;
+                        break;
+                    case "none":
                         return;
-                    }
-                }else{
-                    settings.page = pageText;
+                        // break;
+                    case "go":
+                        var p = parseInt(ele.children(".spage-skip").children("input").val());
+                        if(/^[0-9]*$/.test(p) && p>=1 && p<=pageTatol){
+                            settings.page = p;
+                            pageText = p;
+                        }else{
+                            return;
+                        }
+                        break;
+                    default:
+                        settings.page = pageText;
                 }
-                if(pageText == pageNum){
+                if(pageText == that.pageNum){
                     return;
                 }
-                pageNum = settings.page;
+                that.pageNum = settings.page;
                 that.viewHtml();
-                ele.children(".spage-skip").children("input").val(settings.page);
                 settings.callbackFun(pageText);
             });
             this.element.on('keyup',"input",function(event){
-                if(event.keyCode ==13){
-                    var p = Number(ele.children(".spage-skip").children("input").val());
-                    if(/^[1-9]*$/.test(parseInt(p))){
+                if(event.keyCode == 13){
+                    var p = parseInt(ele.children(".spage-skip").children("input").val());
+                    if(/^[0-9]*$/.test(p) && p>=1 && p<=pageTatol){
                         settings.page = p;
-                        pageNum = p;
+                        that.pageNum = p;
                         that.viewHtml();
-                        ele.children(".spage-skip").children("input").val(settings.page);
                         settings.callbackFun(p);
                     }else{
                         return;
