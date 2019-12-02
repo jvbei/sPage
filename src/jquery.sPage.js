8393f84d8365010b195a73e3d7b443b50194987e
jvbei <yeng525@163.com>
2019-12-02 21:38:58 +0800

    增加省略号快进快退功能，优化双击文本选中效果

--- src/jquery.sPage.js
+++ src/jquery.sPage.js
@@ -1,8 +1,8 @@
 /*  
 *  jQuery分页插件sPage
 *  by 凌晨四点半
-*  20190729
-*  v1.2.0
+*  20191202
+*  v1.2.1
 *  https://github.com/jvbei/sPage
 */
 ; (function ($, window, document, undefined) {
@@ -18,6 +18,7 @@
         showPN: true,//是否显示上下翻页
         prevPage: "上一页",//上翻页按钮文字
         nextPage: "下一页",//下翻页按钮文字
+        fastForward: 0,//快进快退，默认0页
         backFun: function (page) {
             //点击分页按钮回调函数，返回当前页码
         }
@@ -36,7 +37,7 @@
             this.viewHtml();
         },
         creatHtml: function (i) {
-            i == this.settings.page ? this.pageList.push('<span class="active" data-page=' + i + '>' + i + '</span>') : this.pageList.push('<span data-page=' + i + '>' + i + '</span>');
+            i == this.settings.page ? this.pageList.push('<button class="active" data-page=' + i + '>' + i + '</button>') : this.pageList.push('<button data-page=' + i + '>' + i + '</button>');
         },
         viewHtml: function () {
             var settings = this.settings;
@@ -61,7 +62,8 @@
             pageArr.push('<div class="spage-number">');
             this.pageList = [];//页码元素集合，包括上下页
             if (settings.showPN) {
-                settings.page == 1 ? this.pageList.push('<span class="span-disabled" data-page="prev">' + settings.prevPage + '</span>') : this.pageList.push('<span data-page="prev">' + settings.prevPage + '</span>');
+                settings.page == 1 ? this.pageList.push('<button class="button-disabled" data-page="prev">' + settings.prevPage + '</button>') 
+                : this.pageList.push('<button data-page="prev">' + settings.prevPage + '</button>');
             }
             if (pageTatol <= 6) {
                 for (var i = 1; i < pageTatol + 1; i++) {
@@ -72,27 +74,28 @@
                     for (var i = 1; i <= 5; i++) {
                         this.creatHtml(i);
                     }
-                    this.pageList.push('<span data-page="none">...</span><span data-page=' + pageTatol + '>' + pageTatol + '</span>');
+                    this.pageList.push('<button data-page="after" class="spage-after">...</button><button data-page=' + pageTatol + '>' + pageTatol + '</button>');
                 } else if (settings.page > pageTatol - 4) {
-                    this.pageList.push('<span data-page="1">1</span><span data-page="none">...</span>');
+                    this.pageList.push('<button data-page="1">1</button><button data-page="before" class="spage-before">...</button>');
                     for (var i = pageTatol - 4; i <= pageTatol; i++) {
                         this.creatHtml(i);
                     }
                 } else {
-                    this.pageList.push('<span data-page="1">1</span><span data-page="none">...</span>');
+                    this.pageList.push('<button data-page="1">1</button><button data-page="before" class="spage-before">...</button>');
                     for (var i = settings.page - 2; i <= Number(settings.page) + 2; i++) {
                         this.creatHtml(i);
                     }
-                    this.pageList.push('<span data-page="none">...</span><span data-page=' + pageTatol + '>' + pageTatol + '</span>');
+                    this.pageList.push('<button data-page="after" class="spage-after">...</button><button data-page=' + pageTatol + '>' + pageTatol + '</button>');
                 }
             }
             if (settings.showPN) {
-                settings.page == pageTatol ? this.pageList.push('<span class="span-disabled" data-page="next">' + settings.nextPage + '</span>') : this.pageList.push('<span data-page="next">' + settings.nextPage + '</span>');
+                settings.page == pageTatol ? this.pageList.push('<button class="button-disabled" data-page="next">' + settings.nextPage + '</button>') 
+                : this.pageList.push('<button data-page="next">' + settings.nextPage + '</button>');
             }
             pageArr.push(this.pageList.join(''));
             pageArr.push('</div>');
             if (settings.showSkip) {
-                pageArr.push('<div class="spage-skip">跳至&nbsp;<input type="text" value="' + settings.page + '"/>&nbsp;页&nbsp;&nbsp;<span data-page="go">确定</span></div>');
+                pageArr.push('<div class="spage-skip">跳至&nbsp;<input type="text" value="' + settings.page + '"/>&nbsp;页&nbsp;&nbsp;<button data-page="go">确定</button></div>');
             }
             this.element.html(pageArr.join(''));
             this.clickBtn();
@@ -102,8 +105,8 @@
             var settings = this.settings;
             var ele = this.element;
             var pageTatol = this.pageTatol;
-            this.element.off("click", "span");
-            this.element.on("click", "span", function () {
+            this.element.off("click", "button");
+            this.element.on("click", "button", function () {
                 var pageText = $(this).data("page");
                 switch (pageText) {
                     case "prev":
@@ -114,8 +117,14 @@
                         settings.page = Number(settings.page) + 1 <= pageTatol ? Number(settings.page) + 1 : pageTatol;
                         pageText = settings.page;
                         break;
-                    case "none":
-                        return;
+                    case "before":
+                        settings.page = settings.page - settings.fastForward >= 1 ? settings.page - settings.fastForward : 1;
+                        pageText = settings.page;
+                        break;
+                    case "after":
+                        settings.page = Number(settings.page) + Number(settings.fastForward) <= pageTatol ? Number(settings.page) + Number(settings.fastForward) : pageTatol;
+                        pageText = settings.page;
+                        break;
                     case "go":
                         var p = parseInt(ele.find("input").val());
                         if (/^[0-9]*$/.test(p) && p >= 1 && p <= pageTatol) {
@@ -149,6 +158,18 @@
                     }
                 }
             });
+            if(settings.fastForward > 0){
+                ele.find(".spage-after").hover(function(){
+                    $(this).html("&raquo;");
+                },function(){
+                    $(this).html("...");
+                });
+                ele.find(".spage-before").hover(function(){
+                    $(this).html("&laquo;");
+                },function(){
+                    $(this).html("...");
+                });
+            }
         }
     });
     $.fn.sPage = function (options) {
